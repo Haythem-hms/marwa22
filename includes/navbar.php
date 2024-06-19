f2a50b9e6080ed401d7d19fe251c41600a6ed436
Haythem-hms <hemaissihaythem@outlook.com>
2024-06-19 21:55:46 +0100

    Add files via upload

---
+++ includes/navbar.php
@@ -0,0 +1,77 @@
+		<!--header start-->
+		<section class="header">
+			<div class="container">	
+				<div class="header-left">
+					<ul class="pull-left">
+						<li>
+							<a href="#">
+								<i class="fa fa-phone" aria-hidden="true"></i> +992 563 542
+							</a>
+						</li><!--/li-->
+						<li>
+							<a href="#">
+								<i class="fa fa-envelope" aria-hidden="true"></i>
+                                marwa.marzougui@gmail.com
+							</a>
+						</li><!--/li-->
+					</ul><!--/ul-->
+				</div><!--/.header-left -->
+				<div class="header-right pull-right">
+					<ul>
+						
+							
+							
+													
+						<li>
+							<div class="social-icon">
+								<ul>
+									<li><a href="https://www.linkedin.com/in/marwamarzouguiyacoubicoaching/" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+									<li><a href="https://www.instagram.com/maroua.mompreneurcoach/" target="_blank"><i class="fa fa-instagram"></i></a></li>
+									<li><a href="https://www.facebook.com/marwa.marzougui.79" target="_blank"><i class="fa fa-facebook"></i></a></li>
+								</ul><!--/.ul -->
+							</div><!--/.social-icon -->
+						</li><!--/li -->
+					</ul><!--/ul -->
+				</div><!--/.header-right -->
+			</div><!--/.container -->	
+
+		</section><!--/.header-->	
+		<!--header end-->
+		
+		<!--menu start-->
+		<section id="menu">
+			<div class="container">
+				<div class="menubar">
+					<nav class="navbar navbar-default">
+					
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="index.php">
+								<h2 style="color: aliceblue;">MomsProcoaching</h2>
+							</a>
+						</div><!--/.navbar-header -->
+
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<ul class="nav navbar-nav navbar-right">
+								<li class="active"><a href="index.php">Accueil</a></li>
+								<li><a href="about.php">A propos</a></li>
+								<li><a href="service.php">Service</a></li>
+								<li><a href="project.php">Coaching en ligne</a></li>
+								<li><a href="blog.php">Blog</a></li>
+								<li><a href="contact.php">Contact</a></li>
+								
+							</ul><!-- / ul -->
+						</div><!-- /.navbar-collapse -->
+					</nav><!--/nav -->
+				</div><!--/.menubar -->
+			</div><!-- /.container -->
+
+		</section><!--/#menu-->
+		<!--menu end-->
